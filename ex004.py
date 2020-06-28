c24a79f41b892bd3427167ccd54104d0041639e6
jonassignoreti <64449236+jonassignoreti@users.noreply.github.com>
2020-06-28 17:10:49 -0300

    testing a commit

--- ex004.py
+++ ex004.py
@@ -8,3 +8,4 @@ print('É alfanumérico?', a.isalnum())
 print('Está em maiúsculas?', a.isupper())
 print('Está em minúsculas?', a.islower())
 print('Está capitalizada?', a.istitle())
+# testando a comparação
