807e3262b1219903b30e7c006b0b6aaa3e95bfc9
Jonas Signoreti <64449236+jonassignoreti@users.noreply.github.com>
2020-07-13 21:14:18 -0300

    Organizing the project

---
+++ venv/Exercises/ex083.py
@@ -0,0 +1,17 @@
+'''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
+Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.'''
+expr = str(input('Digite a expressão: '))
+test = list()
+for simb in expr:
+    if simb == '(':
+        test.append('(')
+    elif simb == ')':
+        if len(test) > 0:
+            test.pop()
+        else:
+            test.append(')')
+            break
+if len(test) == 0:
+    print('Sua expressão é válida')
+else:
+    print('Sua expressão NÃO é válida')
