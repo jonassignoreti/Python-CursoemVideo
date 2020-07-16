3f787e6d308595c78ac831871344210f8a6d739b
Jonas Signoreti <64449236+jonassignoreti@users.noreply.github.com>
2020-07-15 21:56:29 -0300

    learning python jul-15

--- venv/Exercises/ex088.py
+++ venv/Exercises/ex088.py
@@ -2,3 +2,24 @@
 O programa vai perguntar quantos jogos serão gerados
 e vai sortear 6 números entre 1 e 60 para cada jogo,
 cadastrando tudo em uma lista composta.'''
+from random import randint
+from time import sleep
+jogos = list()
+jogo = list()
+num = 0
+print('$ ' * 10)
+print('GERADOR DE NÚMEROS MEGA-SENA')
+print('$ ' * 10)
+njogos = int(input('Quantos jogos deseja gerar?: '))
+for j in range(0, njogos):
+    while len(jogo) != 6:
+        num = randint(1, 60)
+        if num not in jogo:
+            jogo.append(num)
+    jogo.sort()
+    jogos.append(jogo[:])
+    jogo.clear()
+for j in jogos:
+    print(j)
+    sleep(1)
+print('$ BOA SORTE $')
--- venv/Exercises/ex089.py
+++ venv/Exercises/ex089.py
@@ -2,3 +2,42 @@
 guarde tudo em uma lista composta.
 No final, mostre um boletim contendo a média de cada um
 e permita que o usuário possa mostrar as notas de cada aluno individualmente.'''
+from time import sleep
+aluno = list()
+notas = list()
+lista = list()
+media = a = 0
+while True:
+    nome = str(input('Digite o nome do aluno: ')).strip().capitalize()
+    aluno.append(nome)
+    n1 = float(input('   Nota 1: '))
+    notas.append(n1)
+    n2 = float(input('   Nota 2: '))
+    notas.append(n2)
+    media = (notas[0] + notas[1]) / 2
+    aluno.append(notas[:])
+    aluno.append(media)
+    lista.append(aluno[:])
+    notas.clear()
+    aluno.clear()
+    r = str(input('Deseja continuar? [S/N]: ')).strip().upper()
+    while not (r.__contains__('N') or r.__contains__('S')):
+        print('Código inválido, tente novamente')
+        r = str(input('Deseja continuar? [S para SIM, e N para NÃO]:'))
+    if r == 'N':
+        break
+    print('-' * 30)
+print('-=' * 30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+for i, v in enumerate(lista):
+    print(f'{i:<4}{v[0]:<10}{v[2]:>8.1f}')
+print('-=' * 30)
+while True:
+    a = int(input('Qual aluno você deseja ver as notas? [999 interrompe programa]: '))
+    if a == 999:
+        break
+    if a <= len(lista) - 1:
+        print(f'Notas de {lista[a][0]} são {lista[a][1]}, média final {lista[a][2]}')
+    print('-' * 30)
+print('-=' * 30)
+print('-=' * 3, ' < PROGRAMA FINALIZADO > ', '=-' * 3)
