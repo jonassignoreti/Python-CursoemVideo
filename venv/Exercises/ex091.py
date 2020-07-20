b98af448b0f3a56c8fd1f3c11029eae60c2a3f44
Jonas Signoreti <64449236+jonassignoreti@users.noreply.github.com>
2020-07-19 22:25:24 -0300

    learning Python jul-19

--- venv/Exercises/ex091.py
+++ venv/Exercises/ex091.py
@@ -2,3 +2,19 @@
 Guarde esses resultados em um dicionário.
 No final, coloque esse dicionário em ordem,
 sabendo que o vencedor tirou o maior número no dado.'''
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogo = dict()
+ranking = list()
+print('Valores Sorteados: ')
+for c in range(1, 5):
+    jogo[f'jogador{c}'] = randint(1, 6)
+    sleep(0.5)
+    print(f'Jogador {c} tirou {jogo[f"jogador{c}"]} no dado')
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True) #Pegando valores de um dicionário e ordenando dentro de uma lista de tuplas
+print('-=' * 30)
+print(f'  == RANKING DOS JOGADORES ==')
+for i, v in enumerate(ranking):
+    print(f'    {i + 1}ºlugar: {v[0]} com {v[1]}')
+    sleep(0.5)
--- venv/Exercises/ex092.py
+++ venv/Exercises/ex092.py
@@ -1,4 +1,21 @@
 '''Crie um programa que leia o nome, ano de nascimento e carteira de trabalho
-e cadastr-os (com idade) em um dicionário se por acaso a CTPS for diferente de ZERO,
+e cadastre-os (com idade) em um dicionário e se por acaso a CTPS for diferente de ZERO,
 o dicionário receberá também o ano de contratação e o salário.
 Calcule e acrescente, além da idade, com quantos anos a pessoas vai se aposentar.'''
+from datetime import datetime
+cadastro = dict()
+print('<< CADASTRO DO TRABALHADOR >>')
+cadastro['nome'] = str(input('Nome do trabalhador: ')).strip().title()
+cadastro['idade'] = datetime.now().year - int(input('Ano de Nascimento do trabalhador: '))
+cadastro['ctps'] = int(input('Número da Carteira de Trabalho se houver (caso não tenha, digite 0 (ZERO)): '))
+if cadastro['ctps'] != 0:
+    cadastro['a_contribuição'] = datetime.now().year - int(input('Ano de Contratação: '))
+    cadastro['salario'] = float(input('Salário (R$): '))
+cadastro['aposentar'] = (55 - (cadastro['a_contribuição'])) + cadastro['idade']
+print('\n == CADASTRO TRABALHADOR ==')
+print(f'Nome do Trabalhador: {cadastro["nome"]}')
+print(f'Idade atual do Trabalhador: {cadastro["idade"]}')
+print(f'Número da CTPS do Trabalhador: {cadastro["ctps"]}')
+print(f'Anos de contribuição: {cadastro["a_contribuição"]}')
+print(f'Salário do Trabalhador: R${cadastro["salario"]}')
+print(f'Irá se aposentar com {cadastro["aposentar"]} anos de idade.')
